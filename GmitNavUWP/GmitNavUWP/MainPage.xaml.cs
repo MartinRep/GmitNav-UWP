77e66c33aa5770b81caa1bf8cf0bc3acf6a17cbd
repickym <repickym@gmail.com>
2017-12-12 16:29:14 +0000

    Map Init with Zoom and Gmit coordinates

---
+++ GmitNavUWP/GmitNavUWP/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Services.Maps;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace GmitNavUWP
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            gmitMap.ZoomLevel = 18;
+            gmitMap.LandmarksVisible = false;
+            gmitMap.Center = new Geopoint(new BasicGeoposition()
+            { 
+                Latitude = 53.2785,
+                Longitude = -9.01
+            });
+        }
+    }
+}
